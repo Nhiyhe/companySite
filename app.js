ef9e238aa272000e4c9930f4e2c482367b3dc1de
Nhiyhe Adex <nhiyhe@gmail.com>
2016-05-08 14:33:44 +0100

    Initial commit with root route

---
+++ app.js
@@ -0,0 +1,18 @@
+/// <reference path="./typings/tsd.d.ts" />
+
+var express = require('express');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname+('/node_modules/bootstrap/dist')));
+
+app.get('/',function(req,res){
+    res.render('home');
+})
+
+app.listen(PORT, function(){
+    console.log("Server is running on port " + PORT);
+})
