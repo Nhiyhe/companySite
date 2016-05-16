391e33b8b3ad63a4f55f2573e602b19d2c491df1
Nhiyhe Adex <nhiyhe@gmail.com>
2016-05-16 12:29:33 +0100

    Few changes made

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ var PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname+('/node_modules/bootstrap/dist')));
+app.use(express.static(__dirname+('/node_modules/jquery/dist')));
 
 app.get('/',function(req,res){
     res.render('home');
-})
+});
+
+
 
 app.listen(PORT, function(){
     console.log("Server is running on port " + PORT);
